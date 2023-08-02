e77b1de9339f74576f016c794b30853070e79229
kamlesh-21 <kumar2105k@gmail.com>
2023-08-02 17:26:27 +0530

    updated about and service components

--- vite-project/src/components/About.jsx
+++ vite-project/src/components/About.jsx
@@ -23,10 +23,11 @@ const About = () => {
         </button>
       </div>
       <div className="about-me__body">
-        <p>I'm a passionate web developer and designer based in India. I have a knack for building apps that not only solve real-world problems but also provide delightful user experiences. My specialties include React JS, JavaScript, Responsive Design, and Styled Components.</p> 
-        
-        <p>I hold a Certificate in <strong>Frontend Development</strong>, specializing in React, and I've also completed the <strong>Introduction to Computer Science (CS50x)</strong>, an online CS program from <strong>Harvard</strong> University over the edX platform. My educational background includes a bachelor's degree in Hotel & Hospitality Management from IHM Mumbai, and strong professional experience in Hospitality Operations and Digital Marketing. Oh, and did I mention? I'm also a proud parent to two little kids.</p>
+        <p> I'm a passionate web developer and designer based in India with expertise in React JS, JavaScript, Responsive Design, and Styled Components. Alongside a Certificate in Frontend Development, I've completed Harvard University's Introduction to Computer Science (CS50x). My professional background spans Hospitality Operations and Digital Marketing. As an IT and marketing consultant, I've assisted start-ups with web development and product marketing, enhancing user experience through content management and social media marketing.</p>
+        <p> My expertise extends to the hospitality industry, where I have spent several years in various roles, including restaurant operations, event sales, hotel sales, and restaurant marketing. Furthermore, I have ventured into entrepreneurship with a b2b hotel and restaurant supplies drop-shipping firm, gaining valuable insights into content marketing and social media marketing.</p> 
+        <p>Driven by an unwavering passion to learn and innovate, I'm determined to make a positive impact through web development, marketing, and tackling new challenges. My diverse skill set and entrepreneurial spirit enable me to thrive in the ever-changing digital landscape. Let's collaborate to achieve your business objectives and create exceptional user experiences.</p>
       </div>
+
       <a href="/CV_Kumar_ Front _End_Developer.docx" className="button-image" download>Download Resume</a>
 
     </section>
